41fc73cb0dcaa77d0c5786a0d82d3e4ff1e69a6e
kemal-id <54977402+kemal-id@users.noreply.github.com>
2026-05-29 14:06:38 +0700

    Make modal cancel button

--- src/project-task/modals.js
+++ src/project-task/modals.js
@@ -16,30 +16,44 @@ const modalShow = (modalType) => {
   const body = document.body;
   body.append(modalType);
 
-  const modalbg = document.querySelector(".modal");
+  const mdl = document.querySelector(".modal");
   window.onclick = function (e) {
-    if (e.target == modalbg) {
-      body.removeChild(modalType);
+    if (e.target == mdl) {
+      body.removeChild(mdl);
     }
   };
+
+  const cancelBtn = document.querySelector(".cancel-btn");
+  cancelBtn.addEventListener("click", cancelBtnHandler);
 };
 
+const cancelBtnHandler = (e) => {
+  e.preventDefault();
+  document.body.removeChild(document.querySelector(".modal"));
+  removeEventListener("click", cancelBtnHandler);
+};
+
+
 // Add Project Modal
-const addProjContent = document.createElement("div");
-addProjContent.className = "addProjModal";
-addProjContent.innerHTML = `
-        <form action="" class="add-proj-form">
-          <label for="projName">Project Name:</label>
-          <input type="text" name="projName" id="projName" placeholder="Summer Project" required>
-          <button class="cancel-btn">Cancel</button>
-          <button class="add-proj-btn" type="submit">Add Project</button>
-        </form>
-  `;
+function createAddProjectContent() {
+  const addProjContent = document.createElement("div");
+  addProjContent.className = "addProjModal";
+  addProjContent.innerHTML = `
+          <form action="" class="add-proj-form">
+            <label for="projName">Project Name:</label>
+            <input type="text" name="projName" id="projName" placeholder="Summer Project" required>
+            <button class="cancel-btn">Cancel</button>
+            <button class="add-proj-btn" type="submit">Add Project</button>
+          </form>
+    `;
+
+  return addProjContent;
+}
 
 export const addProjectHandler = (e) => {
   e.preventDefault();
-  // alert("This works");
 
-  const addProjModal = basicModal(addProjContent);
+  const addProjModal = basicModal(createAddProjectContent());
   modalShow(addProjModal);
 };
+
